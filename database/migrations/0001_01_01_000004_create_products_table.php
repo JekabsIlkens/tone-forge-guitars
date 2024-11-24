11133134d56c64b17cfc22b8bebdb1e09b9c48e2
Jekabs Ilkens <jekabs4442@gmail.com>
2024-11-24 02:39:05 +0200

    Categories model, migration and seeding

--- database/migrations/0001_01_01_000004_create_products_table.php
+++ database/migrations/0001_01_01_000004_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->string('image_url');
             $table->integer('price');
             $table->integer('stock');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,21 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->count(7)->create();
-        Product::factory()->count(7)->create();
+
+        $categories = [
+            ['name' => 'Acoustic'],
+            ['name' => 'Electric'],
+            ['name' => 'Bass'],
+        ];
+    
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
+
+        $categoryIds = Category::pluck('id')->toArray();
+
+        Product::factory()->count(7)->create([
+            'category_id' => fn () => $categoryIds[array_rand($categoryIds)],
+        ]);
     }
 }
