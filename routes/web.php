cf16273eb51e0b16ac551cc0619d6dfebb9d5a31
Jekabs Ilkens <jekabs4442@gmail.com>
2024-11-25 02:23:28 +0200

    Routes grouping and cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\Profile\ProfileController;
-use App\Http\Controllers\Profile\PasswordController;
-use App\Http\Controllers\Shop\CategoryController;
-use App\Http\Controllers\Shop\ProductController;
-use App\Http\Controllers\Shop\CartController;
+use App\Http\Controllers\Auth\{RegisterController, LoginController, LogoutController};
+use App\Http\Controllers\Profile\{ProfileController, PasswordController};
+use App\Http\Controllers\Shop\{CategoryController, ProductController, CartController};
 use App\Http\Controllers\Payment\StripeController;
 
-Route::get('/', function () { return inertia('Home'); })->name('home');
+Route::get('/', fn() => inertia('Home'))->name('home');
 
-Route::middleware(['guest'])->group(function () 
-{
+Route::middleware(['guest'])->group(function () {
     Route::get('/register', [RegisterController::class, 'create'])->name('register');
     Route::post('/register', [RegisterController::class, 'store'])->name('register.store')->middleware('throttle:10,1');
 
     Route::get('/login', [LoginController::class, 'create'])->name('login');
     Route::post('/login', [LoginController::class, 'store'])->name('login.store')->middleware('throttle:10,1');
-});
-
-Route::middleware(['auth'])->group(function () 
-{   
-    Route::post('/logout', [LogoutController::class, 'destroy'])->name('logout');
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::patch('/password', [PasswordController::class, 'update'])->name('password.update');
 });
 
 Route::prefix('shop')->group(function () 
@@ -40,12 +23,24 @@ Route::prefix('shop')->group(function ()
     Route::get('/{category}/{product}', [ProductController::class, 'show'])->name('product.show');
 });
 
-Route::middleware(['auth'])->prefix('cart')->group(function () 
-{
-    Route::get('/', [CartController::class, 'index'])->name('cart.index');
-    Route::post('/add', [CartController::class, 'store'])->name('cart.store'); 
-    Route::delete('/remove/{id}', [CartController::class, 'destroy'])->name('cart.destroy');
-});
+Route::middleware(['auth'])->group(function () {   
+    Route::post('/logout', [LogoutController::class, 'destroy'])->name('logout');
 
-Route::post('/stripe', [StripeController::class, 'checkout'])->name('stripe.checkout');
-Route::get('/success', [StripeController::class, 'success'])->name('payment.success');
+    Route::prefix('profile')->name('profile.')->group(function () {
+        Route::get('/', [ProfileController::class, 'edit'])->name('edit');
+        Route::patch('/', [ProfileController::class, 'update'])->name('update');
+        Route::delete('/', [ProfileController::class, 'destroy'])->name('destroy');
+    });
+
+    Route::patch('/password', [PasswordController::class, 'update'])->name('password.update');
+
+    Route::prefix('cart')->name('cart.')->group(function () 
+    {
+        Route::get('/', [CartController::class, 'index'])->name('index');
+        Route::post('/add', [CartController::class, 'store'])->name('store'); 
+        Route::delete('/remove/{id}', [CartController::class, 'destroy'])->name('destroy');
+    });
+
+    Route::post('/stripe', [StripeController::class, 'checkout'])->name('stripe.checkout');
+    Route::get('/success', [StripeController::class, 'success'])->name('payment.success');
+});
