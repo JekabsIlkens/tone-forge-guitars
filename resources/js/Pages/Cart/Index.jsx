0ec239a4973eb4e7b4f590dec042a591aedf9a05
Jekabs Ilkens <jekabs4442@gmail.com>
2024-11-27 15:20:55 +0200

    Removed total price and checkout for when cart is empty

--- resources/js/Pages/Cart/Index.jsx
+++ resources/js/Pages/Cart/Index.jsx
@@ -13,22 +13,34 @@ export default function Index({ cartItems }) {
 
                     <div className="mx-auto">
 
-                        {cartItems.map((item) => (
-                            <CartItemCard 
-                                key={item.id}
-                                id={item.id}
-                                name={item.product.name} 
-                                image={item.product.image_url} 
-                                price={item.product.price / 100} 
-                                quantity={item.quantity}
-                            />
-                        ))}
+                        {cartItems.length ? (
+                            <>
+                                {cartItems.map((item) => (
+                                    <CartItemCard 
+                                        key={item.id}
+                                        id={item.id}
+                                        name={item.product.name} 
+                                        image={item.product.image_url} 
+                                        price={item.product.price / 100} 
+                                        quantity={item.quantity}
+                                    />
+                                ))}
 
-                        <h2 className='title text-start text-gray'>
-                            <span className='text-base_primary'>Total:</span> {calculateTotal()}&#8364;
-                        </h2>
+                                <h2 className='title text-start text-gray'>
+                                    <span className='text-base_primary'>Total:</span> {calculateTotal()}&#8364;
+                                </h2>
 
-                        <CheckoutForm />
+                                <CheckoutForm />
+                            </>
+                        ):(
+                            <>
+                                <div className='bg-white my-4 p-2 text-center rounded-sm shadow-md shadow-base_secondary'>
+                                    <p className="text-center my-4 text-xl font-semibold text-warning-500">
+                                        Your cart is empty...
+                                    </p>
+                                </div>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
