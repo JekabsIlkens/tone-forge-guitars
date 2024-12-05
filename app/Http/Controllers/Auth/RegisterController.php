53e7503455f7d8393c2efff3f61a98d358ea21fd
Jekabs Ilkens <jekabs4442@gmail.com>
2024-12-05 13:53:20 +0200

    Register also logs in user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Services\Auth\RegisterService;
+use App\Services\Auth\LoginService;
 use Illuminate\Http\RedirectResponse;
 use Inertia\Response;
 
 class RegisterController
 {
     protected $registerService;
+    protected $loginService;
 
-    public function __construct(RegisterService $registerService)
+    public function __construct(RegisterService $registerService, LoginService $loginService)
     {
         $this->registerService = $registerService;
+        $this->loginService = $loginService;
     }
 
     public function create(): Response
@@ -24,7 +27,8 @@ class RegisterController
     public function store(RegisterRequest $request): RedirectResponse
     {
         $this->registerService->createUser($request->validated());
+        $this->loginService->attemptLogin($request->validated());
 
-        return redirect()->route('login')->with('success', 'Account created successfully!');
+        return redirect()->route('home')->with('success', 'Account created successfully!');
     }
 }
